8c73c914f122dbc3f12223fc8a4bc6dbf54274da
caianmei <1610143991@qq.com>
2019-08-31 09:34:42 +0800

    创建表

---
+++ build/resources/main/db/migration/V2__create_parking_lot_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `parking_lot` (
+
+`parkingLotId` varchar(15) NOT NULL,
+
+`availablePositionCount` int(3) NULL,
+
+`capatity` int(3) NULL,
+
+`parkingBoyId` varchar(15) NULL,
+
+PRIMARY KEY (`parkingLotId`) 
+
+);
---
+++ src/main/resources/db/migration/V1__create_parking_boy_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `parking_boy` (
+
+`employeeId` varchar(15) NOT NULL,
+
+`name` varchar(10) NULL,
+
+PRIMARY KEY (`employeeId`) 
+
+);
